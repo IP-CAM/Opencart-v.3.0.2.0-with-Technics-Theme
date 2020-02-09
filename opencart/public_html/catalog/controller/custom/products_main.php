fcf4ab1db4f0fb0375d15d68c691e877127cb090
d.sivkov <sivkovdw@yandex.ru>
2020-02-09 23:25:36 +0300

    блок на главной + фикс конфига для storage

--- opencart/public_html/catalog/controller/custom/products_main.php
+++ opencart/public_html/catalog/controller/custom/products_main.php
@@ -2,23 +2,30 @@
 class ControllerCustomProductsMain extends Controller {
 	public function index() {
 		$this->load->model('design/layout');
+		$this->load->model('catalog/product');
+		$this->load->model('catalog/category');
 
-		if (isset($this->request->get['route'])) {
-			$route = (string)$this->request->get['route'];
-		} else {
-			$route = 'common/home';
-		}
+		$filter = array(
+		    'limit' => 9,
+            'start' => 0
+        );
+		$allProducts = $this->model_catalog_product->getProducts($filter);
 
+		$prodCats = array(); // <-- массив "имя товара" => "категории к которым он относится"
 
-        $layout_id = $this->model_design_layout->getLayout($route);
+		foreach ($allProducts as $prod) {
+		    $prodCats[$prod['name']] = $this->model_catalog_product->getCategories($prod['product_id']);
+        }
 
+		$cats = $this->model_catalog_category->getCategories();
 
-		$data['products_main'] = array(
-		    0 => 123,
-            1 => 1231512,
-            2 => 'tratata',
-            3 => 'atafaf'
-            );
+		$cNames = array();
+		foreach ($cats as $cat) {
+		    $cNames[] = $cat['name'];
+        }
+
+		$data['products'] = $allProducts;
+		$data['cat_names'] = $cNames;
 
 		return $this->load->view('custom/products_main', $data);
 	}
