d2f2379672db97d8b02d03941ab86e403f52ed4b
Milifar <48855654+Milifar@users.noreply.github.com>
2020-02-14 00:51:33 +0300

    Новая версия opencart + bd + photo

--- opencart/public_html/catalog/language/en-gb/extension/module/technics_set.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Text
-$_['text_sale']          		 = 'Sale';
-$_['text_quantity']  			 = 'pc.';
-$_['text_chng_btn']  			 = 'Replace item';
-$_['text_item_add']  		     = 'Choose product';
-$_['text_popup_package_title']   = 'Choose a product to replace';
-$_['text_set_added']  		     = 'Set added to cart';
--- opencart/public_html/catalog/language/ru-ru/extension/total/technicssets.php
+++
@@ -1,2 +0,0 @@
-<?php
-$_['text_technicssets'] = 'Скидка на набор';
