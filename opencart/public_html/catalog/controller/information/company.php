451b3066ef8b4ff236689617e5796bb1765f18a9
Dmitrii Sivkov <sivkovdw@yandex.ru>
2020-05-17 15:18:39 +0300

    company fix

--- opencart/public_html/catalog/controller/information/company.php
+++ opencart/public_html/catalog/controller/information/company.php
@@ -30,7 +30,7 @@ class ControllerInformationCompany extends Controller {
         $data['content_top'] = $this->load->controller('common/content_top');
         $data['content_bottom'] = $this->load->controller('common/content_bottom');
         $data['footer'] = $this->load->controller('common/footer');
-        $data['header'] = $this->load->controller('common/header');
+        $data['header'] = $this->load->controller('common/header_company');
 
         // Load the template file and show output
         $this->response->setOutput($this->load->view('information/company', $data));
